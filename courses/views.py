7cbdf9057cc3bbb1f87ca93dc31cf2d4bdc1bfb3
gitkp11 <patilkrunal11.kp@gmail.com>
2020-10-31 18:33:59 +0530

    modified courses to show selected members only

--- courses/views.py
+++ courses/views.py
@@ -2,7 +2,8 @@ from django.views.generic import TemplateView,ListView,DetailView,View
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
-from .models import Course,Branch_of_study
+from .models import Course, Branch_of_study
+from memberships.models import StudentMembership
 
 
 class HomeView(TemplateView):
@@ -32,5 +33,14 @@ class CourseListView(ListView):
 class CourseDetailView(DetailView):
     context_object_name = 'course'
     template_name = 'courses/course_detail.html'
-    model = Course
+    queryset = Course.objects.all()
+    #model = Course
 
+
+    def get_context_data(self, **kwargs):
+        context = super(CourseDetailView, self).get_context_data(**kwargs)
+        context['student_membership'] = StudentMembership.objects.all()
+        context['courses'] = Course.objects.all()
+        context['branches'] = Branch_of_study.objects.all()
+
+        return context
